6135cd5227c4b95e63b276b4bf38f2286f9fad4f
larrys1 <54840488+larrys1@users.noreply.github.com>
2019-09-30 19:16:19 +0300

    Almost

--- src/index.js
+++ src/index.js
@@ -4,21 +4,12 @@ var turn = 1;
 var gameOver = 0;
 
 var field = [
-  ["", "", "h", "", ""],
+  ["", "", "", "", ""],
   ["", "", "", "", ""],
   ["", "", "", "", ""],
   ["", "", "", "", ""],
   ["", "", "", "", ""]
 ];
-
-// access gameField
-console.log(field[0][2]);
-//for (var i in field) {
-//console.log("row " + i);
-//for (var j in field[i]) {
-//  console.log(" " + field[i][j]);
-// }
-//}
 
 if (document.readyState !== "loading") {
   // Document ready, executing
@@ -37,11 +28,11 @@ function initializeCode() {
   const boardi = document.getElementById("board");
 
   // cells creation
-  for (var i = 0; i <= 5; i++) {
+  for (var i = 0; i <= 4; i++) {
     // table row creation
     var row = document.createElement("tr");
 
-    for (var j = 0; j < 5; j++) {
+    for (var j = 0; j <= 4; j++) {
       // create element <td> and text node
       //Make text node the contents of <td> element
       // put <td> at end of the table row
@@ -79,20 +70,22 @@ function initializeCode() {
   //);
 }
 
-document.getElementById("turn").innerHTML = "<h2>Player 1: x, Player 2:o</h2>";
+document.getElementById("info").innerHTML = "<h2>Player 1: x, Player 2:o</h2>";
+document.getElementById("turn").innerHTML = "<h2>Turn of Player 1 (x)</h2>";
 
 function move() {
-  if ((gameOver = 0 || turn === 1)) {
+  if (gameOver === 0 && turn === 1 && this.innerHTML === "") {
     this.innerHTML = "x";
-    //console.log();
-    //field((parseInt(this.id[0], 10)[parseInt(this.id[1], 10)] = "x"));
     field[parseInt(this.id[0], 10)][parseInt(this.id[1], 10)] = "x";
-    //printField();
-    checkWinner();
+    document.getElementById("turn").innerHTML = "<h2>Turn of Player 2 (o)</h2>";
     turn = 2;
-
-    //var i = this.id;
-    //console.log(this.id[0]+this.id[1]);
+    checkWinner();
+  } else if (gameOver === 0 && turn === 2 && this.innerHTML === "") {
+    this.innerHTML = "o";
+    field[parseInt(this.id[0], 10)][parseInt(this.id[1], 10)] = "o";
+    document.getElementById("turn").innerHTML = "<h2>Turn of Player 1 (x)</h2>";
+    turn = 1;
+    checkWinner();
   }
 }
 
@@ -111,15 +104,23 @@ var checkWinner = function() {
 
   for (var i = 0; i < 5; i++) {
     for (var j = 0; j < 5; j++) {
-      if (field[i][j] === 1) {
+      if (field[i][j] === "x") {
         p1++;
-      } else if (field[i][j] === 2) {
+      } else if (field[i][j] === "o") {
         p2++;
       }
       if (p1 === 5) {
-        return "player 1 won";
+        document.getElementById("turn").innerHTML =
+          "<h2>Player 1 (x) wins!</h2>";
+        alert("Player 1 won!");
+        gameOver = 1;
+        //return "player 1 won";
       } else if (p2 === 5) {
-        return "player 2 won";
+        document.getElementById("turn").innerHTML =
+          "<h2>Player 2 (o) wins!</h2>";
+        alert("Player 2 won!");
+        gameOver = 1;
+        //return "player 2 won";
       }
     }
     p1 = 0;
@@ -127,47 +128,70 @@ var checkWinner = function() {
   }
   for (var j = 0; j < 5; j++) {
     for (var i = 0; i < 5; i++) {
-      if (field[i][j] === 1) {
+      if (field[i][j] === "x") {
         p1++;
-      } else if (field[i][j] === 2) {
+      } else if (field[i][j] === "o") {
         p2++;
       }
       if (p1 === 5) {
-        return "player 1 won";
+        document.getElementById("turn").innerHTML =
+          "<h2>Player 1 (x) wins!</h2>";
+        alert("Player 1 won!");
+        gameOver = 1;
+        //return "player 1 won";
       } else if (p2 === 5) {
-        return "player 2 won";
+        document.getElementById("turn").innerHTML =
+          "<h2>Player 2 (o) wins!</h2>";
+        alert("Player 2 won!");
+        gameOver = 1;
+        //return "player 2 won";
       }
     }
     p1 = 0;
     p2 = 0;
   }
   for (var j = 0; j < 5; j++) {
-    if (field[j][j] === 1) {
+    if (field[j][j] === "x") {
       p1++;
-    } else if (field[j][j] === 2) {
+    } else if (field[j][j] === "o") {
       p2++;
     }
     if (p1 === 5) {
-      return "player 1 won";
+      document.getElementById("turn").innerHTML = "<h2>Player 1 (x) wins!</h2>";
+      alert("Player 1 won!");
+      gameOver = 1;
+      //return "player 1 won";
     } else if (p2 === 5) {
-      return "player 2 won";
+      document.getElementById("turn").innerHTML = "<h2>Player 2 (o) wins!</h2>";
+      alert("Player 2 won!");
+      gameOver = 1;
+      //return "player 2 won";
     }
   }
   p1 = 0;
   p2 = 0;
   for (var k = 0; k < 5; k++) {
-    if (field[2 - k][0 + k] === 1) {
+    if (field[2 - k][0 + k] === "x") {
       p1++;
-    } else if (field[2 - k][0 + k] === 2) {
+    } else if (field[2 - k][0 + k] === "o") {
       p2++;
     }
     if (p1 === 5) {
-      return "player 1 won";
+      document.getElementById("turn").innerHTML = "<h2>Player 1 (x) wins!</h2>";
+      alert("Player 1 won!");
+      gameOver = 1;
+
+      //return "player 1 won";
     } else if (p2 === 5) {
-      return "player 2 won";
+      document.getElementById("turn").innerHTML = "<h2>Player 2 (o) wins!</h2>";
+      alert("Player 2 won!");
+      gameOver = 1;
+      //return "player 2 won";
     }
   }
   p1 = 0;
   p2 = 0;
+  if (gameOver === 1) {
+  }
   return "no winner";
 };
