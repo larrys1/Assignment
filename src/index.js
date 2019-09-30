e3fd862cb1c106510b2cb268bb5dfd6fc3e78800
larrys1 <54840488+larrys1@users.noreply.github.com>
2019-09-30 13:05:45 +0300

    next

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
 import "./styles.css";
 
+var turn = 1;
+var gameOver = 0;
+
+var gameField = [
+  [0, 0, 0, 0, 0],
+  [0, 2, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0]
+];
+
+// access gameField
+// console.log(gameField[1][1]);
+
+if (document.readyState !== "loading") {
+  // Document ready, executing
+  console.log("Document ready, executing");
+  initializeCode();
+} else {
+  document.addEventListener("DOMContentLoaded", function() {
+    // Document was not ready, executing when loaded
+    console.log("Document ready, executing after a wait");
+    initializeCode();
+  });
+}
+
+function initializeCode() {
+  console.log("Initializing");
+  const boardi = document.getElementById("board");
+
+  //  button.addEventListener("mousedown", event => {
+  //    buttonActivities();
+  //    event.stopPropagation();
+  // }
+  //);
+}
+
 document.getElementById("board").innerHTML = `
-<h1>Assignment 1: Tic tac toe</h1>
 <table>
   <tr>
   <td>d</td>
@@ -34,3 +70,5 @@ document.getElementById("board").innerHTML = `
 
 </table>
 `;
+
+document.getElementById("turn").innerHTML = "<h2>testihng</h2>";
